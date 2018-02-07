fd929a432c5042fe335c66fec267454b0dd2a80b
Oded Sharon <odedshr@gmail.com>
2018-02-07 07:32:58 +0000

    Adding README, icon and temp

---
+++ README.md
@@ -0,0 +1,13 @@
+A Very simple weather App
+====================
+
+# Installation
+1. clone 
+
+# Usage
+
+# TODO:
+1. diplay data
+2. style data
+3. unit test
+4. complete doc
--- main.js
+++ main.js
@@ -1,20 +1,21 @@
 const key = 'f20a73d3467ff08b53d3ad37ef781115',
-    cityName = 'EdinburghXX';
+    cityName = 'Edinburgh';
 
 function App(){
     this.getData()
         .then(data => {
             if (data.cod === '200')  {
-                this.printCityInfo(data.city);
+                this.appendCityInfo(document.body, data.city);
+                this.appendWeatherInfo(document.body, data.list);
             } else {
-                this.printError(data);
+                this.appendError(document.body, data);
             }
         });
 }
 
 App.prototype = {
     getData: function getData() {
-        return fetch('https://api.openweathermap.org/data/2.5/forecast?q='+ cityName +'&appid=' + key)
+        return fetch('https://api.openweathermap.org/data/2.5/forecast?units=metric&q='+ cityName +'&appid=' + key)
             .then(response => response.json());
     },
 
@@ -22,22 +23,42 @@ App.prototype = {
         return document.importNode(document.getElementById(templateName).content, true);
     },
 
-    printCityInfo: function (city) {
-        console.log(city);
+    appendCityInfo: function (target, city) {
         let clone = this.getTemplate('template-cityData');
         clone.querySelector('.cityName').innerHTML = city.name;
         clone.querySelector('.countryName').innerHTML = city.country;
         
-        document.body.appendChild(clone);
+        target.appendChild(clone);
     },
 
-    printError: function (error) {
+    appendWeatherInfo: function (target, items) {
+        let clone = this.getTemplate('template-weatherList'),
+            listDomElement = clone.querySelector('.weatherInformation');
+        
+        items.forEach(item => {
+            this.appendWeatherItem(listDomElement, item);
+        });
+
+        target.appendChild(clone);
+    },
+
+    appendWeatherItem: function (target, item) {
+        let clone = this.getTemplate('template-weatherItem');
+
+        console.log(item);
+        clone.querySelector('.weatherIcon').src = 'http://openweathermap.org/img/w/' + item.weather[0].icon + '.png';
+        clone.querySelector('.tempValue').innerHTML = item.main.temp;
+
+        target.appendChild(clone);
+    },
+
+    appendError: function (target, error) {
         let clone = this.getTemplate('template-error');
         
         clone.querySelector('.errorMessage').innerHTML = error.message;
         clone.querySelector('.errorCode').innerHTML = error.cod;
         
-        document.body.appendChild(clone);
+        target.appendChild(clone);
     }
 };
 
